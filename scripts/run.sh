65a511f5f193de07dd27e86f867e5334e2e2671c
Vinh Truong <vinhphuc.truong@gmail.com>
2024-12-26 14:55:52 +0200

    Basic setup

---
+++ scripts/run.sh
@@ -0,0 +1,12 @@
+set -e
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+ROOT_DIR="$( cd "$SCRIPT_DIR/.." && pwd )"
+BIN_DIR="$ROOT_DIR/build/Debug"
+echo "Script location: $SCRIPT_DIR"
+echo "Root location: $ROOT_DIR"
+pushd $ROOT_DIR
+cmake -S . -B build
+cmake --build build
+$BIN_DIR/stormcloud.exe
+popd
---
+++ src/main.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main() {
+    printf("Hello, stormcloud!\n");
+    return 0;
+}
