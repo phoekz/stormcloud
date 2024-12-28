36e5add91ca48bb4d4f5bd1507e1ab0c8fc4f18c
Vinh Truong <vinhphuc.truong@gmail.com>
2024-12-28 12:17:02 +0200

    ESC to exit

--- src/main.c
+++ src/main.c
@@ -776,6 +776,11 @@ SDL_AppResult SDL_AppEvent(void* appstate, SDL_Event* event) {
     if (event->type == SDL_EVENT_QUIT) {
         return SDL_APP_SUCCESS;
     }
+    if (event->type == SDL_EVENT_KEY_DOWN) {
+        if (event->key.key == SDLK_ESCAPE) {
+            return SDL_APP_SUCCESS;
+        }
+    }
 
     return SDL_APP_CONTINUE;
 }
